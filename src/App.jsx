1283129bb1c7f6338ffc37ae1a4fbafecfb2e67a
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-07-27 13:38:37 +0530

    initialize routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import About from "./Pages/About";
+import Home from "./Pages/Home";
 import SmoothScroll from "./Components/SmoothScroll";
+import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <SmoothScroll>
-      <div className="w-full m-auto">
-        <About />
+      <div className="w-full">
+        <Routes>
+          <Route path="about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </SmoothScroll>
   );
