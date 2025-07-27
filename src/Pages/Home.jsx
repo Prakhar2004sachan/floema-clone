8e87dce4d21174bd9750a24aba3235c105922c4e
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-07-27 20:12:09 +0530

    Home done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <div className="bg-[#C97164] w-ful h-screen overflow-hidden"></div>;
 }
 
 export default Home
