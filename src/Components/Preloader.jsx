363b8f2663598a3eef4cf43e7988a38861637834
Prakhar Sachan <sachan2004prakhar@gmail.com>
2025-07-29 15:17:36 +0530

    making preloader responsive

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Preloader({ percentage }) {
   return (
     <div className="w-full h-screen bg-[#C97164] grid grid-rows-[1fr_auto] place-items-center text-white">
-      <p className="text-center font-main text-[3rem]">
+      <p className="text-center mt-[10rem] font-main md:text-[3rem]">
         The surprise of what is possible <br /> to make with a simple <br /> and
         thin thread.
       </p>
